328f685b2527a3058d99f9a020a71f07ab980795
Claudio Canepa <ccanepacc@gmail.com>
2020-06-10 18:29:40 -0300

    converted to py3 convertconfig.py

--- data/themes/tools/convertconfig.py
+++ data/themes/tools/convertconfig.py
@@ -6,15 +6,15 @@ import sys
 try:
     path = sys.argv[1]
 except:
-    print "usage: convertconfig.py config.txt"
-    print ""
-    print "Converts the old PGU theme config.txt format into the style.ini format"
-    print ""
+    print("usage: convertconfig.py config.txt")
+    print("")
+    print("Converts the old PGU theme config.txt format into the style.ini format")
+    print("")
     sys.exit(1)
 
 styleByClass = collections.defaultdict(dict)
 
-fd = file(path, "r")
+fd = open(path, "r")
 for line in fd.readlines():
     line = line.strip()
     if (not line): continue
@@ -25,14 +25,14 @@ for line in fd.readlines():
         attr = args[1]
         values = " ".join(args[2:])
     except:
-        print "Invalid line: %s" % line
+        print("Invalid line: %s" % line)
         sys.exit()
     styleByClass[name][attr] = values
 fd.close()
 
 for klass in sorted(styleByClass.keys()):
-    print "[%s]" % klass
+    print("[%s]" % klass)
     for attr in sorted(styleByClass[klass].keys()):
-        print "%s = %s" % (attr, styleByClass[klass][attr])
-    print ""
+        print("%s = %s" % (attr, styleByClass[klass][attr]))
+    print("")
 
